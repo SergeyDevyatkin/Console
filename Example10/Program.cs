f339c1e3addd9e87da9a280d3818c07a66ef47fe
Sergey Devyatkin <lut17@mail.ru>
2022-09-18 17:43:01 +0300

    Задания с Лекции2

---
+++ Example10/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 11, 51, 52, 47, 52, 11, 25, 68, 41 };
+
+int n = array.Length;
+int find = 52;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
